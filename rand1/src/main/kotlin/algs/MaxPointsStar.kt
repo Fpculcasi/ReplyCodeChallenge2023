5462c4fe412462f70a79a440cca73634290d9c6e
Davide Sorcelli <d.sorcelli@reply.it>
2023-03-09 19:57:00 +0100

    Fixed max points algs

--- rand1/src/main/kotlin/algs/MaxPointsStar.kt
+++ rand1/src/main/kotlin/algs/MaxPointsStar.kt
@@ -4,6 +4,7 @@ import cache.StarPointsCacheKey
 import cache.StartPointCache
 import model.CircularMatrix
 import model.Coordinate
+import model.Direction
 
 fun findMaxPointsStar(
     area: CircularMatrix,
@@ -36,20 +37,51 @@ fun calculateStarSum(
     center: Coordinate
 ): Int {
 
+    var currentX = center.x
+    repeat(range - 1) {
+        currentX = area.move(from = Coordinate(currentX, 0), direction = Direction.U).second.x
+    }
+
     var sum = 0
+    for (i in 0 until range * 2) {
 
-    val xLowerBound = center.x - (range - 1)
-    val xUpperBound = center.x + (range - 1)
+        if (i <= range - 1) {
 
-    val yLowerBound = center.y - (range - 1)
-    val yUpperBound = center.y + (range - 1)
+            sum += area.pointsAt(Coordinate(currentX, center.y))
 
-    for (i in xLowerBound..xUpperBound) {
-        for (j in yLowerBound..yUpperBound) {
-            sum += area.pointsAt(Coordinate(i, j))
+            // left
+            var leftYOffset = center.y
+            for (j in 0 until i) {
+                leftYOffset = area.move(from = Coordinate(currentX, leftYOffset), Direction.L).second.y
+                sum += area.pointsAt(Coordinate(currentX, leftYOffset))
+            }
+
+            // right
+            var rightYOffset = center.y
+            for (j in 0 until i) {
+                rightYOffset = area.move(from = Coordinate(currentX, leftYOffset), Direction.R).second.y
+                sum += area.pointsAt(Coordinate(currentX, rightYOffset))
+            }
+        } else {
+            sum += area.pointsAt(Coordinate(currentX, center.y))
+
+            // left
+            var leftYOffset = center.y
+            for (j in (range - 1) until i) {
+                leftYOffset = area.move(from = Coordinate(currentX, leftYOffset), Direction.L).second.y
+                sum += area.pointsAt(Coordinate(currentX, leftYOffset))
+            }
+
+            // right
+            var rightYOffset = center.y
+            for (j in (range - 1) until i) {
+                rightYOffset = area.move(from = Coordinate(currentX, leftYOffset), Direction.R).second.y
+                sum += area.pointsAt(Coordinate(currentX, rightYOffset))
+            }
         }
+
+        currentX = area.move(from = Coordinate(x = currentX, y = center.y), Direction.D).second.y
     }
 
     return sum
-
 }
